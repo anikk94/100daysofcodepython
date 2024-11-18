780e35bab8086927f2175959bea8ec214ee80b4e
Anirudh Krishnan Komaralingam <anirudh.krishnankomaralingam@nist.gov>
2024-11-18 16:44:57 -0500

    day1learn code added

--- day1/day1learn.py
+++ day1/day1learn.py
@@ -1,2 +1,24 @@
 print("Hello, world!")
 
+print()
+
+print("1. line 1")
+print("2. line 2")
+print("3. line 3")
+print("4. line 4")
+print("5. line 5")
+
+
+print()
+
+
+print("1. line 1\n2. line 2\n3. line 3\n4. line 4\n5. line 5")
+
+print()
+
+print("hello " + "ani")
+print("hello" + " " + "ani")
+
+print()
+
+
